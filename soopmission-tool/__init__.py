84b3eca86b876b4cc4d37b74790f48c9dadca4b2
Charlotte Pierce <charlotte@malformed-bits.com>
2023-01-12 12:33:04 +1100

    Added upload blueprint page.
    Takes a task id and shows the upload requirements from the json data.
    
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- soopmission-tool/__init__.py
+++ soopmission-tool/__init__.py
@@ -1,18 +1,17 @@
 import os
 
-from flask import Flask, render_template, url_for, json
+from flask import Flask, render_template
 
 # the application factory function
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
+    from . import task_format
+    app.register_blueprint(task_format.bp)
+
     @app.route('/')
     def index():
-        # task_data = json.load(open(url_for('static', filename='data/tasks.json')))
-        # print(task_data)
-        tasks_file_url = os.path.join(app.root_path, 'static/data/tasks.json')
-        task_data = json.load(open(tasks_file_url))
-        print(task_data)
+        task_data = task_format.get_task_data()
         return render_template('index.html', task_data=task_data)
 
     return app
---
+++ soopmission-tool/task_format.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, json, current_app
+
+import os
+
+bp = Blueprint('task_format', __name__, url_prefix='/upload')
+
+@bp.route('/<task_id>', methods=('GET', 'POST'))
+def upload_task(task_id):
+    task_data = get_task_data(task_id)
+    name = task_data['name']
+    upload_requirements = task_data['upload_requirements']
+    return render_template('task_format/upload.html', task_id=task_id, name=name, upload_requirements=upload_requirements)
+
+
+def get_task_data(task_id=None):
+    '''Read the json task data and return it.
+    
+    Returns all the data by default, or the data for a specific task if 
+    `task_id` is provided.
+    '''
+    tasks_file_url = os.path.join(current_app.root_path, 'static/data/tasks.json')
+    all_task_data = json.load(open(tasks_file_url))
+    if task_id:
+        return all_task_data[task_id]
+    
+    return all_task_data
+    
